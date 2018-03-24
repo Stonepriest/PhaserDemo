f10151dcda4d9ee3699c50a37c58e4b4d5ca6ebe
David Priester <stonepriest@hotmail.com>
2018-03-24 18:33:02 -0400

    Added camera, boundary, and arcade physics

--- state0.js
+++ state0.js
@@ -5,28 +5,44 @@ demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
         game.load.image('elf', 'assets/sprites/PiskelElf.png');
+        game.load.image('treeBg', 'assets/backgrounds/phaserBg.png');
     },
     create: function(){
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#00ff99';
         console.log('state0');
         
         addChangeStateEventListeners();
+        game.world.setBounds(0, 0, 2813, 1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        var treeBg = game.add.sprite(0,0, 'treeBg');
         
         elf = game.add.sprite(centerX, centerY, 'elf');
         //elf.anchor.x = 0.5;
         //elf.anchor.y = 0.5;
         elf.anchor.setTo(0.5, 0.5);
+        elf.scale.setTo(0.8, 0.8);
+        game.physics.enable(elf);
+        elf.body.collideWorldBounds = true;
+        
+        game.camera.follow(elf);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
     },
     update: function(){
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            elf.scale.setTo(0.8, 0.8);
             elf.x += speed;
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            elf.scale.setTo(-0.8, 0.8);
             elf.x -= speed;
         }
         if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
             elf.y -= speed;
+            if(elf.y < 368){
+                elf.y = 368;
+            }
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
             elf.y += speed;
